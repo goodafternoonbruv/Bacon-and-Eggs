622ed715e7abe1de6d9c5bf0c439994a780aada0
Aiden Nickel <145699504+5CentCode@users.noreply.github.com>
2023-09-29 13:25:22 -0400

    Started a link list
    Started a link list with a small amount of data

--- sketch.js
+++ sketch.js
@@ -1,13 +1,12 @@
 let userManager;
 function setup() {
-  
+  //load data
   createCanvas(windowWidth, windowHeight);
   windowResized();
   uploadData();
 }
 
-function windowResized()
-{
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
 
@@ -15,7 +14,18 @@ function draw() {
 
 }
 
-function uploadData()
-{
-  userManager = new List();
+function uploadData() {
+  let user = new User("Jamal", "Murry", 28, 0, [true, true, true, true, false, false, false, false, false, false]);
+
+  userManager = new List(user);
+  userManager.curr = userManager.first;
+
+  user = new User("Brent", "Nickel", 63, 0, [false, false, false, false, false, false, true, true, false, true]);
+
+  while (userManager.curr != null)
+  {
+    userManager.curr = userManager.curr.next;
+  }
+
+  user = userManager.curr.next;
 }
