c528bcf9796aed1ed5740c78ff37f4dde9426747
goodafternoonbruv <145598776+goodafternoonbruv@users.noreply.github.com>
2023-09-30 00:56:43 -0400

    made a kinda good lookign signup form data entry is for future max

---
+++ test apps/test.php
@@ -0,0 +1,19 @@
+<?php
+$fieldA = $_POST["textnames"];
+$keys = array('id_number', 'first_name'); //FORM ELEMENTS
+$csv_line = array();
+foreach($keys as $key){
+
+array_push($csv_line, '' . $_GET[$key]);
+}
+
+$fname = 'test.csv' //FILENAME
+$csv_line = implode(',', $csv_line);
+if(!file_exists($fname)){$csv_line = "\r\n" . $csv_line;}
+$fcontent = $csv_line;
+fwrite($fcon,$csv_line);
+fclose($fcon);
+
+
+
+?>
