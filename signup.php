6c6f8f2ef5d87ec6358f4660ec6f756e627ac100
HandyAndy7 <eppan@students.dsbn.org>
2023-10-21 14:13:51 -0400

    csv just barely works a bit

--- signup.php
+++ signup.php
@@ -3,4 +3,16 @@
 
 var_dump($_POST);
 
+$userdata = [
+    $_POST["firstname"],
+    $_POST["lastname"],
+    $_POST["username"],
+    $_POST["email"],
+    $_POST["gender"],
+    $_POST["sexuality"],
+    $_POST["hobby"]
+];
 
+$fp = fopen('users.csv', 'w');
+
+fputcsv($fp, $userdata);
