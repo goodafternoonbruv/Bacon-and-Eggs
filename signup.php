122be15b353e4eaa0d7a47e8943f000842eb94cb
HandyAndy7 <eppan@students.dsbn.org>
2023-10-29 18:35:48 -0400

    WINWWINWINWINWIWINWINWIWNININ
    WWWWWWWWWWWWWWWW

--- signup.php
+++ signup.php
@@ -10,7 +10,16 @@ $userdata = [
     $_POST["email"],
     $_POST["gender"],
     $_POST["interest"],
-    json_encode($_POST["hobbies"])
+    $_POST["hobby1"] ? "True" : "False" ,
+    $_POST["hobby2"] ? "True" : "False" ,
+    $_POST["hobby3"] ? "True" : "False" ,
+    $_POST["hobby4"] ? "True" : "False" ,
+    $_POST["hobby5"] ? "True" : "False" ,
+    $_POST["hobby6"] ? "True" : "False" ,
+    $_POST["hobby7"] ? "True" : "False" ,
+    $_POST["hobby8"] ? "True" : "False" ,
+    $_POST["hobby9"] ? "True" : "False" ,
+    $_POST["hobby10"] ? "True" : "False" ,
 ];
 
 $fp = fopen('MatchMaker Responses.csv', 'a');
